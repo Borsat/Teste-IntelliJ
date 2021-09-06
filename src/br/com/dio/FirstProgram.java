3e4545065abd060a11e5e65d0f049eff7b0c716e
Rodrigo F. Borsato <rodrigo.borsato20@hotmail.com>
2021-09-06 16:17:56 -0300

    criando class livro

--- src/br/com/dio/FirstProgram.java
+++ src/br/com/dio/FirstProgram.java
@@ -9,9 +9,49 @@ public class FirstProgram {
 
         System.out.println(dog);
 
+        Livro livro = new Livro("invencivel", 300);
+
+        System.out.println(livro);
+
         /*int a = 5;
         int b = 3;
         System.out.println("By World" + (a+b));*/
     }
 }
+
+class Livro {
+    private String nome;
+    private Integer numpaginas;
+
+    public Livro (String nome, Integer numpaginas) {
+        this.nome = nome;
+        this.numpaginas = numpaginas;
+        }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpaginas() {
+        return numpaginas;
+    }
+
+    public void setNumpaginas(Integer numpaginas) {
+        this.numpaginas = numpaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numpaginas=" + numpaginas +
+                '}';
+    }
+}
+
+
 
